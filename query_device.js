ebf93109a7f0b679dede4ff59f7ee29a3f72ac2d
NightFlight <ecwh73@gmail.com>
2021-12-02 16:21:59 -0500

    Create query_device.js

---
+++ query_device.js
@@ -0,0 +1,1 @@
+javascript:%20(function()%20{%20var%20val=%20prompt("Enter%20Device%20Query#","");%20if%20(val)%20location="https://instance.domain.com:8443/mobileDevices.html?queryType=Mobile%20Devices&query="+escape(val);%20})()
