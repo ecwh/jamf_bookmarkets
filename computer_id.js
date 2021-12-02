93e49c3d2c1e8d12ad1f8376cfd03092dc20e836
NightFlight <ecwh73@gmail.com>
2021-12-02 16:20:28 -0500

    Create computer_id.js

---
+++ computer_id.js
@@ -0,0 +1,1 @@
+javascript:%20(function()%20{%20var%20val=%20prompt("Enter%20Computer%20ID#","");%20if%20(val)%20location="https://instance.domain.com:8443/computers.html?id="+escape(val)+"&o=r";%20})()
